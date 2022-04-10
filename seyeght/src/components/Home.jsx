30b21bf15ef72ebba6e3269a1bbdc62e6d63e44e
hanzuo123 <hanzuo123@gmail.com>
2022-04-10 11:12:06 -0400

    Add map to index.html

--- seyeght/src/components/Home.jsx
+++ seyeght/src/components/Home.jsx
@@ -2,15 +2,7 @@ import React from "react";
 import logo from "../logo.png"
 function Home() {
     return (
-        <div className="App">
-  
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Welcome to sEYEght. We are the future of peer-to-peer, crowdsourced detection and reporting of unknown or unwanted anomalies worldwide.
-          </p>
-  
-        </header>
+      <div className="App">
       </div>
     );
 }
